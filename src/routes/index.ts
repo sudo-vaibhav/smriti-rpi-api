20e62c72f86c32a7134d4e34190599bc66c42f99
Vaibhav <mailvaibhavchopra@gmail.com>
2020-11-08 03:16:11 +0530

    added more route config (not created controllers yet)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import largePersonGroupsController from '../controllers/largePersonGroups';
-// import peopleController from '../controllers/people';
+import peopleController from '../controllers/people';
 const router = Router();
 
 router.post('/largePersonGroups', largePersonGroupsController.create);
 router.route('/largePersonGroups/').get(largePersonGroupsController.get).delete(largePersonGroupsController.delete);
 
 router.post('/largepersongroups/:largePersonGroupId/train', largePersonGroupsController.train);
+
+router.post('/largepersongroups/persons', peopleController.add);
+router.get('/largepersongroups/persons', peopleController.listPeople);
+router.post('/largepersongroups/persons/:personId/persistedfaces', peopleController.addFace);
+router.delete('/largepersongroups/persons/:personId', peopleController.delete);
+
+router.get('/largepersongroups/identify', peopleController.identify);
 
 export default router;
